8744c6cccb89331463a4d4ea5c54be77b7648885
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-08 08:52:14 +0200

    add new js lesson

---
+++ Course_1/Course-Lessons/3_Control_Flow/3-4_while.js
@@ -0,0 +1,17 @@
+// while loops are used to execute a block of code repeatedly as long as a specified condition is true.
+
+let i = 0;
+while (i <= 5) {
+    console.log('Hello World!', i);
+    i++;
+}
+
+// Countdown
+
+let number = 5;
+
+while (number > 0) {
+  console.log(number);
+  number--;
+}
+
