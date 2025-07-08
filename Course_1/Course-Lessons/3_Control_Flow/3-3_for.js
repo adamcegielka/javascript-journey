578b6e878212b141f92fb90460d31031e4a33853
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-08 08:48:19 +0200

    add examples

--- Course_1/Course-Lessons/3_Control_Flow/3-3_for.js
+++ Course_1/Course-Lessons/3_Control_Flow/3-3_for.js
@@ -13,3 +13,32 @@ for (let i = 1; i <= 5; i++) {
 for (let i = 5; i >= 1; i--) {
     if (i % 2 !== 0) console.log(i);
 }
+
+// Skip every 2
+
+for (let i = 0; i <= 10; i += 2) {
+  console.log('Skip every 2', i);
+}
+
+// Iteration over an array
+
+const fruit = ['apple', 'banana', 'pear'];
+
+for (let i = 0; i < fruit.length; i++) {
+  console.log(fruit[i]);
+}
+
+// Iteration and condition checking
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i + ' - is even');
+  }
+}
+
+// Breaking the loop (break)
+
+for (let i = 0; i < 10; i++) {
+  if (i === 5) break;
+  console.log(i);
+}
