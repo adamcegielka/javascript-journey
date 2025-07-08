010159f97b0a7f40af0fa0f85028e801915ba74b
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-08 08:29:53 +0200

    add new js lesson

---
+++ Course_1/Course-Lessons/3_Control_Flow/3-3_for.js
@@ -0,0 +1,15 @@
+// for -> i = 0; i < 5
+
+for (let i = 0; i < 5; i++) {
+    console.log('Hello World!', i);
+}
+
+// for -> i = 1; i <= 5
+
+for (let i = 1; i <= 5; i++) {
+    console.log('Hi Adam!', i);
+}
+
+for (let i = 5; i >= 1; i--) {
+    if (i % 2 !== 0) console.log(i);
+}
