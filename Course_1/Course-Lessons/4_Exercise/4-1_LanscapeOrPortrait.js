b0a12cb4d641e8bba91705ad5f2a3e648457e48f
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-11 08:53:02 +0200

    add new js lesson - exercise

---
+++ Course_1/Course-Lessons/4_Exercise/4-1_LanscapeOrPortrait.js
@@ -0,0 +1,12 @@
+// Exercise - Landscape or Portrait
+
+console.log(isLandscape(800, 600)); // true
+console.log(isLandscape(600, 800)); // false
+
+function isLandscape(width, height) {
+  if (width > height) {
+    return true;
+  } else {
+    return false;
+  }
+}   
