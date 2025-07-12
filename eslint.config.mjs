480bd2d0b897f3517eca0d2e9032a716dd5fb793
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-12 10:21:19 +0200

    refactor eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,33 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    ignores: ["node_modules/", "tips/", "utils/", "README.md"], // Add ignored paths here
+    languageOptions: {
+      parser: tsParser,
+      globals: {
+        ...globals.browser,
+        process: true,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...pluginJs.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      "@typescript-eslint/no-unused-expressions": [
+        "error",
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
+    },
+  },
 ];
