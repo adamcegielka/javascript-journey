c7766fcf3d72a0e689dc2059de18799c5ba5421b
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-12 10:46:06 +0200

    add new js lesson - exercise

---
+++ Course_1/Course-Lessons/4_Exercise/4-4_EvenAndOdd.js
@@ -0,0 +1,10 @@
+// Exercise - Even and Odd Numbers
+
+showNumbers(10);
+
+function showNumbers(limit) {
+    for (let i = 0; i <= limit; i++) {
+        const message = i % 2 === 0 ? 'EVEN' : 'ODD';
+        console.log(i, message);
+    }
+}
