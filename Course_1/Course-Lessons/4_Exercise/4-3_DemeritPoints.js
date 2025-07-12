cc7e487814464e7085babc8dde6826cbb09bf67e
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-12 10:03:57 +0200

    add new js lesson - exercise

---
+++ Course_1/Course-Lessons/4_Exercise/4-3_DemeritPoints.js
@@ -0,0 +1,24 @@
+// Demerit Points
+
+//  Speed Limit = 70
+// 5 -> 1 demerit point
+// MATH.floor(1.3)
+// 12 or more demerit points -> License suspended
+
+checkSpeed(130);
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const demeritPointPer5 = 5;
+
+    if (speed < speedLimit + demeritPointPer5) {
+        console.log('Ok');
+        return
+    }
+    
+    const points = Math.floor((speed - speedLimit) / demeritPointPer5);
+    if (points >= 12)
+        console.log('License suspended');
+    else
+        console.log(`Points: ${points}`);
+}
