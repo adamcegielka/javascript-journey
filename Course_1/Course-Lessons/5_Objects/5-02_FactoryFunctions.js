013afd5b90165461601bc23a4d825980cea39f37
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-15 20:38:18 +0200

    refactor by prettier

--- Course_1/Course-Lessons/5_Objects/5-02_FactoryFunctions.js
+++ Course_1/Course-Lessons/5_Objects/5-02_FactoryFunctions.js
@@ -7,15 +7,17 @@ function createPerson(name, age) {
   return {
     name: name,
     age: age,
-    greet: function() {
-      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
-    }
+    greet: function () {
+      console.log(
+        `Hello, my name is ${this.name} and I am ${this.age} years old.`,
+      );
+    },
   };
 }
 
 // Example usage:
-const person1 = createPerson('Alice', 30);
-const person2 = createPerson('Bob', 25);
+const person1 = createPerson("Alice", 30);
+const person2 = createPerson("Bob", 25);
 person1.greet(); // Output: Hello, my name is Alice and I am 30 years old.
 person2.greet(); // Output: Hello, my name is Bob and I am 25
 
