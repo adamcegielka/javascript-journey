deecb36373e20a694d90beaeaf5d32f690bcf5c2
Adam Cegiełka <adamcegielka@tlen.pl>
2025-07-15 20:57:06 +0200

    add new js lesson - Objects

---
+++ Course_1/Course-Lessons/5_Objects/5-05_ConstructorProperty.js
@@ -0,0 +1,12 @@
+// Objects - Constructor Property
+
+// This lesson introduces the constructor property of objects in JavaScript.
+// The constructor property is a reference to the function that created the instance of the object.
+
+const person = {
+  name: "Alice",
+  age: 25,
+  city: "Los Angeles",
+};
+
+console.log(person.constructor); // Output: [Function: Object]
